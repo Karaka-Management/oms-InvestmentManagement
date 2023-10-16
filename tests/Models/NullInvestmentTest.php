2063ce5d536e50a64f9b0c84137bb480748e7305
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-10-16 22:25:58 +0000

    auto generate NullTests

---
+++ tests/Models/NullInvestmentTest.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Jingga
+ *
+ * PHP Version 8.1
+ *
+ * @package   tests
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 2.0
+ * @version   1.0.0
+ * @link      https://jingga.app
+ */
+declare(strict_types=1);
+
+namespace Modules\InvestmentManagement\tests\Models;
+
+use Modules\InvestmentManagement\Models\NullInvestment;
+
+/**
+ * @internal
+ */
+final class NullInvestmentTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @covers Modules\InvestmentManagement\Models\NullInvestment
+     * @group framework
+     */
+    public function testNull() : void
+    {
+        self::assertInstanceOf('\Modules\InvestmentManagement\Models\Investment', new NullInvestment());
+    }
+
+    /**
+     * @covers Modules\InvestmentManagement\Models\NullInvestment
+     * @group framework
+     */
+    public function testId() : void
+    {
+        $null = new NullInvestment(2);
+        self::assertEquals(2, $null->getId());
+    }
+
+    /**
+     * @covers Modules\InvestmentManagement\Models\NullInvestment
+     * @group framework
+     */
+    public function testJsonSerialize() : void
+    {
+        $null = new NullInvestment(2);
+        self::assertEquals(['id' => 2], $null);
+    }
+}
