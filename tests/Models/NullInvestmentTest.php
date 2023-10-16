1b21b4007cbd2bb0a5173f415bec4fcb1bbce094
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-10-16 23:39:39 +0000

    Add test cases for null models

--- tests/Models/NullInvestmentTest.php
+++ tests/Models/NullInvestmentTest.php
@@ -23,7 +23,7 @@ final class NullInvestmentTest extends \PHPUnit\Framework\TestCase
 {
     /**
      * @covers Modules\InvestmentManagement\Models\NullInvestment
-     * @group framework
+     * @group module
      */
     public function testNull() : void
     {
@@ -32,7 +32,7 @@ final class NullInvestmentTest extends \PHPUnit\Framework\TestCase
 
     /**
      * @covers Modules\InvestmentManagement\Models\NullInvestment
-     * @group framework
+     * @group module
      */
     public function testId() : void
     {
@@ -42,7 +42,7 @@ final class NullInvestmentTest extends \PHPUnit\Framework\TestCase
 
     /**
      * @covers Modules\InvestmentManagement\Models\NullInvestment
-     * @group framework
+     * @group module
      */
     public function testJsonSerialize() : void
     {
