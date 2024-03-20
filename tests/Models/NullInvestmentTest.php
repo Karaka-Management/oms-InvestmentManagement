16436b08f084ff1cb383d7129e1c45196ff64b36
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2024-03-20 03:00:24 +0000

    fix tests

--- tests/Models/NullInvestmentTest.php
+++ tests/Models/NullInvestmentTest.php
@@ -22,7 +22,7 @@ use Modules\InvestmentManagement\Models\NullInvestment;
 final class NullInvestmentTest extends \PHPUnit\Framework\TestCase
 {
     /**
-     * @covers Modules\InvestmentManagement\Models\NullInvestment
+     * @covers \Modules\InvestmentManagement\Models\NullInvestment
      * @group module
      */
     public function testNull() : void
@@ -31,7 +31,7 @@ final class NullInvestmentTest extends \PHPUnit\Framework\TestCase
     }
 
     /**
-     * @covers Modules\InvestmentManagement\Models\NullInvestment
+     * @covers \Modules\InvestmentManagement\Models\NullInvestment
      * @group module
      */
     public function testId() : void
@@ -41,7 +41,7 @@ final class NullInvestmentTest extends \PHPUnit\Framework\TestCase
     }
 
     /**
-     * @covers Modules\InvestmentManagement\Models\NullInvestment
+     * @covers \Modules\InvestmentManagement\Models\NullInvestment
      * @group module
      */
     public function testJsonSerialize() : void
